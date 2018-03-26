6fb531bf3bca0554ecec21d360783cfeb9751366
kukujiabo <kukujiabo@163.com>
2018-03-26 16:05:20 +0800

    123

--- public/init.php
+++ public/init.php
@@ -31,3 +31,25 @@ if (\PhalApi\DI()->debug) {
 
 // 翻译语言包设定
 \PhalApi\SL('zh_cn');
+
+if (!function_exists('getallheaders')) { 
+  
+  function getallheaders() { 
+
+    $headers = []; 
+
+    foreach ($_SERVER as $name => $value) { 
+
+      if (substr($name, 0, 5) == 'HTTP_') {
+
+        $headers[str_replace(' ', '-', str_replace('_', ' ', substr($name, 5)))] = $value; 
+
+      } 
+
+    } 
+
+    return $headers; 
+
+  } 
+
+}
